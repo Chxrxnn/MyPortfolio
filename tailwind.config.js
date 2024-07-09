8d0a3bfebb3e7e41489a2868e477730639186b5b
Chxrxnn <deep963496@gmail.com>
2024-07-09 06:32:10 +0530

    from local

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        fnt:["Lobster","cursive"],
+        fnt2:["Dancing Script","cursive"],
+        fnt3:["Playwrite DE Grund","cursive"],
+        fnt4:["Teko"]
+      }
+    },
+  },
+  plugins: [],
+}
+
